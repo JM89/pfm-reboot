90361435fa59fa72a62b97530b05350cdbbd60df
JM89 <jessica.mottner@gmail.com>
2021-03-28 19:46:38 +0100

    Add Forms to create resources

--- web-ui/playground/my-react-app-with-ts/src/components/Savings.tsx
+++ web-ui/playground/my-react-app-with-ts/src/components/Savings.tsx
@@ -1,6 +1,7 @@
 // src/components/Savings.tsx
 
-import React from "react";
+
+import { useReducer, useState, useEffect } from 'react';
 import axios from "axios";
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -17,43 +18,150 @@ const useStyles = makeStyles({
     },
 });
 
-function Savings() {
-    const [savings, setSavings] = React.useState<any[]>([])
+const formReducer = function (state: any, event: any) {
+    if (event.reset) {
+        return {
+            amount: '',
+            transfer_date: '',
+            currency: '',
+            src_account: '',
+            dest_account: ''
+        }
+    }
+    return {
+        ...state,
+        [event.name]: event.value
+    }
+}
 
-    React.useEffect(() => {
+function Savings() {
+    const [savings, setSavings] = useState<any[]>([])
+    const [bankAccounts, setBankAccounts] = useState<any[]>([])
+
+    const [formData, setFormData] = useReducer(formReducer, {});
+    const [submitting, setSubmitting] = useState<boolean>(false);
+    const handleSubmit = function (event: any) {
+        event.preventDefault();
+        setSubmitting(true);
+
+        axios.post("http://127.0.0.1:5000/savings", {
+            amount: formData['amount'],
+            transfer_date: formData['transfer_date'],
+            currency: formData['currency'],
+            src_account: formData['src_account'],
+            dest_account: formData['dest_account']
+        }).then(function (response) {
+            axios
+                .get("http://127.0.0.1:5000/savings")
+                .then((response) => setSavings(response.data));
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+        setTimeout(() => {
+            setSubmitting(false);
+            setFormData({
+                reset: true
+            })
+        }, 3000)
+    }
+
+    const handleChange = function (event: any) {
+        setFormData({
+            name: event.target.name,
+            value: event.target.value,
+        });
+    }
+
+    useEffect(() => {
         axios
             .get("http://127.0.0.1:5000/savings")
             .then((response) => setSavings(response.data))
             .catch(err => {
                 console.log(err);
             });
+        axios
+            .get("http://127.0.0.1:5000/bank-accounts")
+            .then((response) => setBankAccounts(response.data));
     }, []);
 
     const classes = useStyles();
 
     return (
-        <TableContainer component={Paper} >
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Transfer Date</TableCell>
-                        <TableCell>Amount</TableCell>
-                        <TableCell>Source Account</TableCell>
-                        <TableCell>Destination Account</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {savings.map((row) => (
-                        <TableRow key={row.id}>
-                            <TableCell>{row.transfer_date}</TableCell>
-                            <TableCell>{row.amount} {row.currency}</TableCell>
-                            <TableCell>{row.src_account.name == null ? row.src_account.code : row.src_account.name + " (" + row.src_account.description + ")"}</TableCell>
-                            <TableCell>{row.dest_account.name == null ? row.dest_account.code : row.dest_account.name + " (" + row.src_account.description + ")"}</TableCell>
+        <div className="wrapper">
+            <h3>Create</h3>
+            {submitting &&
+                <div>
+                    You are submitting the following:
+                    <ul>
+                        {Object.entries(formData).map(([name, value]) => (
+                            <li key={name}><strong>{name}</strong>:{value}</li>
+                        ))}
+                    </ul>
+                </div>
+            }
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <label>
+                        <p>Source Bank Account:</p>
+                        <select name="src_account" onChange={handleChange}>
+                            <option value="">--Please choose an option--</option>
+                            {Object.entries(bankAccounts).map(([key, value]) =>
+                                <option value={key}>{value.name}</option>
+                            )}
+                        </select>
+                    </label>
+                    <label>
+                        <p>Destination Bank Account:</p>
+                        <select name="dest_account" onChange={handleChange}>
+                            <option value="">--Please choose an option--</option>
+                            {Object.entries(bankAccounts).map(([key, value]) =>
+                                <option value={key}>{value.name}</option>
+                            )}
+                        </select>
+                    </label>
+                    <label>
+                        <p>Transfer Date</p>
+                        <input name="transfer_date" onChange={handleChange} value={formData.transfer_date || ''} />
+                    </label>
+                    <label>
+                        <p>Amount</p>
+                        <input name="amount" onChange={handleChange} value={formData.amount || ''} />
+                    </label>
+                    <label>
+                        <p>Currency:</p>
+                        <select name="currency" onChange={handleChange} >
+                            <option value="GBP">GBP</option>
+                            <option value="EUR">EUR</option>
+                        </select>
+                    </label>
+                </fieldset>
+                <button type="submit">Submit</button>
+            </form>
+            <TableContainer component={Paper} >
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Transfer Date</TableCell>
+                            <TableCell>Amount</TableCell>
+                            <TableCell>Source Account</TableCell>
+                            <TableCell>Destination Account</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {savings.map((row) => (
+                            <TableRow key={row.id}>
+                                <TableCell>{row.transfer_date}</TableCell>
+                                <TableCell>{row.amount} {row.currency}</TableCell>
+                                <TableCell>{row.src_account.name == null ? row.src_account.code : row.src_account.name + " (" + row.src_account.description + ")"}</TableCell>
+                                <TableCell>{row.dest_account.name == null ? row.dest_account.code : row.dest_account.name + " (" + row.src_account.description + ")"}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
     );
 }
 

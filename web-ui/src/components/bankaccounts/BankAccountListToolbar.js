ec6bc05fd1b1a74c68b7bf2194bffde3f36a2d46
JM89 <jessica.mottner@gmail.com>
2021-03-30 22:07:17 +0100

    Add popup

--- web-ui/src/components/bankaccounts/BankAccountListToolbar.js
+++ web-ui/src/components/bankaccounts/BankAccountListToolbar.js
@@ -8,8 +8,33 @@ import {
   SvgIcon
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+let code = '';
+let name = '';
+let description = '';
+
+function handleSubmit() {
+  console.log(code);
+  console.log(name);
+  console.log(description);
+}
+
+function handleChange(event) {
+  if (event.target.name === 'code') {
+    code = event.target.value;
+  }
+  if (event.target.name === 'name') {
+    name = event.target.value;
+  }
+  if (event.target.name === 'description') {
+    description = event.target.value;
+  }
+}
 
 const BankAccountListToolbar = (props) => (
+
   <Box {...props}>
     <Box
       sx={{
@@ -17,12 +42,46 @@ const BankAccountListToolbar = (props) => (
         justifyContent: 'flex-end'
       }}
     >
-      <Button
-        color="primary"
-        variant="contained"
+      <Popup
+        trigger={<Button color="primary" variant="contained">Add bank account</Button>}
+        modal
+        nested
       >
-        Add bank account
-      </Button>
+        {(close) => (
+          <div className="modal">
+            <button type="button" className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="header">New bank account</div>
+            <div className="content">
+              <form>
+                <label htmlFor="code">
+                  <p>Code</p>
+                  <input id="code" name="code" onChange={handleChange} />
+                </label>
+                <label htmlFor="name">
+                  <p>Name</p>
+                  <input id="name" name="name" onChange={handleChange} />
+                </label>
+                <label htmlFor="description">
+                  <p>Description</p>
+                  <input id="description" name="description" onChange={handleChange} />
+                </label>
+                <br />
+                <br />
+                <div className="actions">
+                  <Button className="submit" color="primary" variant="contained" onClick={() => { handleSubmit(); close(); }}>
+                    Submit
+                  </Button>
+                  <Button className="button" onClick={() => { console.log('modal closed '); close(); }}>
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </Popup>
     </Box>
     <Box sx={{ mt: 3 }}>
       <Card>
--- web-ui/src/components/savings/SavingsListToolbar.js
+++ web-ui/src/components/savings/SavingsListToolbar.js
@@ -8,6 +8,30 @@ import {
   SvgIcon
 } from '@material-ui/core';
 import { Search as SearchIcon } from 'react-feather';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+let code = '';
+let name = '';
+let description = '';
+
+function handleSubmit() {
+  console.log(code);
+  console.log(name);
+  console.log(description);
+}
+
+function handleChange(event) {
+  if (event.target.name === 'code') {
+    code = event.target.value;
+  }
+  if (event.target.name === 'name') {
+    name = event.target.value;
+  }
+  if (event.target.name === 'description') {
+    description = event.target.value;
+  }
+}
 
 const SavingsListToolbar = (props) => (
   <Box {...props}>
@@ -17,12 +41,46 @@ const SavingsListToolbar = (props) => (
         justifyContent: 'flex-end'
       }}
     >
-      <Button
-        color="primary"
-        variant="contained"
+      <Popup
+        trigger={<Button color="primary" variant="contained">Add savings</Button>}
+        modal
+        nested
       >
-        Add savings
-      </Button>
+        {(close) => (
+          <div className="modal">
+            <button type="button" className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="header">New savings</div>
+            <div className="content">
+              <form>
+                <label htmlFor="code">
+                  <p>Code</p>
+                  <input id="code" name="code" onChange={handleChange} />
+                </label>
+                <label htmlFor="name">
+                  <p>Name</p>
+                  <input id="name" name="name" onChange={handleChange} />
+                </label>
+                <label htmlFor="description">
+                  <p>Description</p>
+                  <input id="description" name="description" onChange={handleChange} />
+                </label>
+                <br />
+                <br />
+                <div className="actions">
+                  <Button className="submit" color="primary" variant="contained" onClick={() => { handleSubmit(); close(); }}>
+                    Submit
+                  </Button>
+                  <Button className="button" onClick={() => { console.log('modal closed '); close(); }}>
+                    Cancel
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </Popup>
     </Box>
     <Box sx={{ mt: 3 }}>
       <Card>
